299b9a56e7db53e63c82c23d1619e6f00eff8181
Kourtney <barnes.kourt@gmail.com>
2022-03-07 16:59:56 -0500

    kourtney

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    const num = _.random(0,20);
+    console.log(num);
+
+    const greet = _.once(() =>{
+        console.log('hello')
+    });
+    greet();
+    greet();
+    
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case'/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case'/attractions':
+            path += 'attractions.html';
+            res.statusCode = 200;
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
