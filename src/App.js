83cbdf1d3b2fb08990e26f5e5aeff2f14a4c50e5
Alexander Evras <evrasalexander@gmail.com>
2022-03-08 09:55:17 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import Home from './components/Home';
 function App() {
   return (
     <Router>
     <div className="App">
-     <h1>Capstone</h1>
-     <p>Test</p>
+      <Route path='/' component={Home}/>
     </div>
     </Router>
   );
