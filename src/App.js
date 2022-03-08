790691919d6dfcd037b322c1389c5d05e15f7e2e
Alexander Evras <evrasalexander@gmail.com>
2022-03-08 11:19:44 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
-import Home from './components/Home';
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <Route path='/' component={Home}/>
-    </div>
-    </Router>
-  );
-}
 
+// import from components
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+ class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+         <div className='App'>
+          <Switch>
+
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={Contact}/>
+
+          </Switch>
+         </div>
+      </BrowserRouter>
+    )
+  }
+}
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Contact = () => {
+    
+    return (
+        <div className='container'>
+        
+        <h4>Contact</h4>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore nam vitae ipsum quaerat natus ducimus maxime, eum laborum, eligendi nostrum provident veritatis placeat modi, soluta architecto pariatur dolores ullam!</p>
+
+        </div>
+  )
+}
+
+export default Contact;
