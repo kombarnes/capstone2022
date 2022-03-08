1f779897a11c9e7c562d167a8f9e02658c6b1f2b
Alexander Evras <evrasalexander@gmail.com>
2022-03-08 09:34:29 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
 function App() {
   return (
+    <Router>
     <div className="App">
      <h1>Capstone</h1>
      <p>Test</p>
     </div>
+    </Router>
   );
 }
 
